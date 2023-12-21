d12e7b07a478082a3727da0a40f0dce3d9ee232f
KozerT <kozer.vizastaff@gmail.com>
2023-12-21 23:13:22 +0100

    initial commit

---
+++ react-01/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>Fundamental React concepts.</p>
+      </header>
+      <main>
+        <h2>Get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
